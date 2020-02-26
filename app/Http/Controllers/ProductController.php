1ecd7a157d471b6b7531723e7b9a193528f4dafa
ing.halvarado.7s <ing.halvarado.7s@gmail.com>
2020-02-26 04:44:06 -0430

    Creación de layout.blade.php y list.blade.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
     /**
@@ -13,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
